1eb99b6891583560bfc5706e2c0ec713c840925e
ganesh123 <ravulaganeshece@gmail.com>
2025-03-25 15:31:33 +0530

    new

---
+++ m8.java
@@ -0,0 +1,202 @@
+//accept a scholarship as input and take female scholarship as 15000 and male as 20,000//
+class M8
+ {
+   public static void main(String []args)
+    {
+      /* String gender="female";
+       int fee=50000;
+       int scholarship=0;
+       if(gender=="male")
+        {
+              scholarship=15000;
+              fee=fee-scholarship;
+              System.out.println(fee);
+        }
+       else
+        {
+               scholarship=20000;
+               fee=fee-scholarship;
+               System.out.println(fee);
+        }*/
+
+
+
+
+//accept a day as input and print the output//
+           /* int day=5;
+            if(day==1)
+               System.out.println("monday");
+            else if(day==2)
+               System.out.println("tuesday");
+            else if(day==3)
+               System.out.println("wednesday");
+            else if(day==4)
+               System.out.println("thursday");
+            else if(day==5)
+               System.out.println("friday");
+             else if(day==6)
+               System.out.println("saturday");
+             else if(day==7)
+                System.out.println("sunday");
+              else
+                System.out.println("invalid day");*/
+
+           
+
+  //accept a letter as input and find the output//    
+                /* int letter='c';
+                 if(letter=='a')
+                   {
+                      System.out.println("vowel");
+                   }
+                   else if(letter=='e')
+                   {
+                     System.out.println("vowel");
+                   }
+                   else if(letter=='i')
+                   {
+                     System.out.println("vowel");
+                   }
+                   else if(letter=='o')
+                   {
+                     System.out.println("vowel");
+                   }
+                   else if(letter=='u')
+                   {
+                     System.out.println("vowel");
+                   }
+                   else 
+                   {
+                      System.out.println("consonent");
+                   }*/
+
+
+
+//accept a letter as input and find output is vowel or consonent and print it in a logical operator//
+                   /*  int letter='o';
+                     if(letter=='a'||letter=='e'||letter=='i'||letter=='o'||letter=='u')
+                      {
+                         System.out.println("vowels");
+                      }
+                      else
+                       {
+                         System.out.println("consonent");
+                        }*/
+
+
+
+//accept a month as input and take the output in a logical operator//
+                        /* int month=1;
+                         if(month==1||month==3||month==5||month==7||month==9||month==12)
+                          {
+                             System.out.println("31 days");
+                          }
+                         else if(month==4||month==6||month==8||month==10||month==11)
+                          {
+                              System.out.println("30 days");
+                          }
+                         else if(month==2)
+                          {
+                                System.out.println("28/29 days");
+                           }
+                          else
+                           {
+                               System.out.println("invalid days");
+                           }*/
+
+
+
+
+//write a programme to check whether a person is major or minor or senior citizen or kid// 
+                             /* int age=12;
+                              if(age<0)
+                               {
+                                 System.out.println("invalid negatve input");
+                               }
+                              else if(age<10)
+                               {
+                                  System.out.println("kid");
+                               }
+                              else if(age<18)
+                               {
+                                   System.out.println("minor");
+                               }
+                              else if(age<60)
+                                {
+                                   System.out.println("major");
+                                }
+                              else if(age<=100)
+                                 {
+                                    System.out.println("senior citizen");
+                                 }
+                               else
+                                 {
+                                    System.out.println("invalid positive number");
+                                 }*/
+
+
+
+//take a marks as input and in each subject less than 35 as fail in each subject and lessthan 40 as fail and less than 50 as e grade and less than 60 as d grade and less than 70 as c grade and less than 80 as b grade and less than 90 as a grade and where have 100 as a+ grade // 
+                                  /* int m1=55,m2=56,m3=55,m4=95,m5=87,m6=38,max=600;
+                                  double total=m1+m2+m3+m4+m5+m6;       
+                                  double percentage=total/max*100;  
+				   if(m1<35||m2<35||m3<35||m4<35||m5<35||m6<35)
+				    {
+					System.out.println("fail");
+				     }
+                                  else if(percentage<40)
+                                    {
+                                       System.out.println("fail");
+                                     }
+                                    else if(percentage<50)
+                                     {
+                                       System.out.println("e garde");
+                                     }
+                                    else if(percentage<60)
+                                     {
+                                        System.out.println("d grade");
+                                     }
+                                    else if(percentage<70)
+                                     {
+                                         System.out.println("c grade");
+                                      }
+                                    else if(percentage<80)
+                                      {
+                                          System.out.println("b grade");
+                                      }
+                                     else if(percentage<90)
+                                       {
+                                          System.out.println("a grade");
+                                        }
+                                      else 
+                                        {
+                                           System.out.println("distinction");
+                                         }*/
+
+
+
+
+//accept a letter as input and find the vowels and consonent in a ternariy operator//
+                                        /*char letter='o';             System.out.println(letter=='a'||letter=='e'||letter=='i'||letter=='o'||letter=='u'? "vowels" : "conosonent");*/
+
+
+
+
+
+//accept a number as input and find it's as perfect square or not//
+                                         /*  int num1=18;
+                                           int num2=9;
+                                          System.out.println( num1<num2 ? num1 : num2);*/
+                                         /* int n=17;
+                                           double res=Math.sqrt(n);
+                                           int t=(int)(res);
+                                           if((t*t)==n)
+                                            {
+                                               System.out.println(n +" is a perfect square");
+                                            }
+                                          else
+                                            {
+                                               System.out.println(n +" is not a perfect square");
+                                            }*/
+     }
+ }
